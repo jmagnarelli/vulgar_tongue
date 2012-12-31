d2d7305358674253082b9768b09393c62d8ae11e
James Magnarelli <jmagnare@gmail.com>
2012-12-31 17:58:18 -0500

    Code now functional. Needs logging and more documentation.

--- src/vulgar.py
+++ src/vulgar.py
@@ -2,8 +2,8 @@
 
 """vulgar.py: A very naughty twitter bot"""
 
-__author__="James Magnarelli"
-__version__="pre-alpha"
+__author__ = "James Magnarelli"
+__version__ = "pre-alpha"
 
 
 #==============================================================================
@@ -11,13 +11,22 @@ __version__="pre-alpha"
 # All rights reserved. Not licensed for use without express permission.
 #==============================================================================
 # TODO (jmagnare): Docstrings everywhere
+# TODO (jmagnare): Add logging
 
 from argparse import ArgumentParser
-import json
 import random
 import re
 import sqlite3
-import twitter
+from twitter import Twitter, OAuth
+
+
+class Vulgar_Exception(Exception):
+
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return repr(self.value)
 
 
 class Text_Utils(object):
@@ -26,7 +35,7 @@ class Text_Utils(object):
     _VULGAR_DICT_PATH = "../data/vulgar.txt"
 
     @staticmethod
-    def get_dictionary_dict(self):
+    def get_dictionary_dict():
         # Open the dict file for reading ('with' will close when finished)
         with open(Text_Utils._VULGAR_DICT_PATH) as f:
             # Read it into memory
@@ -58,11 +67,11 @@ class Text_Utils(object):
 
         return vulgar_dict
 
+
 class DB_Utils(object):
     """Class to handle retrieving words from the vulgar dictionary"""
 
     _VULGAR_DB_PATH = "../data/vulgardb"
-    _VULGAR_TABLE_NAME = "vulgardict"
 
     def __init__(self):
         """Initialize a WordGetter, loading the dictionary file"""
@@ -72,16 +81,14 @@ class DB_Utils(object):
         c = conn.cursor()
 
         # Create the table
-        c.execute("CREATE TABLE ? (term text, used integer, def text)",
-             DB_Utils._VULGAR_TABLE_NAME)
+        c.execute("CREATE TABLE vulgar (term text, used integer, def text)")
 
         # Get the dictionary into memory
         vulgar_dict = Text_Utils.get_dictionary_dict()
 
         # Insert all that fancy booklearnin'
-        for t, d in vulgar_dict:
-            c.execute("INSERT INTO ? (term, used, def) VALUES (?, 0, ?)",
-                      DB_Utils._VULGAR_TABLE_NAME, t, d)
+        c.executemany("INSERT INTO vulgar (term, used, def) VALUES (?, 0, ?)",
+                      vulgar_dict.items())
 
         # Save changes
         conn.commit()
@@ -89,45 +96,50 @@ class DB_Utils(object):
     def _handle_used_db(self, conn):
         """Marks all terms in dictionary db as unused"""
         c = conn.cursor()
-        c.execute("UPDATE ? SET used=0")
+        c.execute("UPDATE vulgar SET used=0")
 
         # Save changes
         conn.commit()
 
     def _open_vulgar_db(self):
 
-        conn = sqlite3.connect(DB_Utils._VULGAR_DB_NAME)
+        conn = sqlite3.connect(DB_Utils._VULGAR_DB_PATH)
 
         # Check whether vulgar db already exists
 
         c = conn.cursor()
         c.execute("SELECT name FROM sqlite_master WHERE type='table'\
-        AND name=?", DB_Utils._VULGAR_TABLE_NAME)
+        AND name='vulgar'")
         if not c.fetchall():
             self._build_new_db(conn)
-
         return conn
 
     def get_new_word(self):
         """Return an unused (word, definition) from the dict, and note use"""
 
         # Open our dictionary
-        conn = self._open_vulgar_db
+        conn = self._open_vulgar_db()
+        c = conn.cursor()
+
+        # Determine number of remaining unused words
+
+        c.execute("SELECT COUNT (*) FROM vulgar WHERE used = 0")
+        num_unused = c.fetchall()[0]
 
         # Handle case where all words have been used
-        self._handle_used_db(conn)
+        if num_unused == 0:
+            self._handle_used_db(conn)
 
         # Build collection of unused words
 
-        c = conn.cursor()
-        c.execute("SELECT term, def FROM dict WHERE used = 0")
+        c.execute("SELECT term, def FROM vulgar WHERE used = 0")
         unused_terms = c.fetchall()
 
         # Choose a random word
         chosen_term = random.choice(list(unused_terms))
 
         # Note its use, and update dictionary file
-        c.execute("UPDATE dict SET used=1 WHERE term=?", chosen_term[0])
+        c.execute("UPDATE vulgar SET used=1 WHERE term=?", [chosen_term[0]])
 
         # Save changes and close
         conn.commit()
@@ -136,15 +148,43 @@ class DB_Utils(object):
         return chosen_term
 
 
-class WordPoster(object):
+class Word_Poster(object):
     """Class to handle posting a retrieved word"""
-    # TODO (jmagnare): write this
-    pass
+
+    _TWITTER_NAME = "Vulgar Brit"
+
+    # Let's say we're posting from London
+    _MY_LATITUDE = 51.5171
+    _MY_LONGITUDE = -0.1062
+
+    def __init__(self, key, csecret, token, asecret):
+
+        # Establish a connection to twitter
+        self.conn = Twitter(auth=OAuth(token, asecret, key, csecret))
+
+        # Check to make sure we connected successfully
+
+        settings = self.conn.account.verify_credentials()
+        if settings['name'] != Word_Poster._TWITTER_NAME:
+            raise Vulgar_Exception("Failed to connect to Twitter")
+
+    def post(self, term_pairing):
+
+        # Append term and definition
+        post_text_full = term_pairing[0] + " " + term_pairing[1]
+
+        # Truncate for twitter
+        post_text_trunc = post_text_full[:140]
+
+        # Post the thing
+        self.conn.statuses.update(status=post_text_trunc,
+                                  lat=Word_Poster._MY_LATITUDE,
+                                  long=Word_Poster._MY_LONGITUDE)
 
 
 def post_word(key, csecret, token, asecret):
     """Post a new vulgar word with the given credentials"""
-    poster = WordPoster(key, csecret, token, asecret)
+    poster = Word_Poster(key, csecret, token, asecret)
     db = DB_Utils()
 
     # Get the daily posting (word, def) for today
